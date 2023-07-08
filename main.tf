24dc9ce024b9fa4bbcc111f7767f146c180df1bd
itsnek <nikosgr9917@gmail.com>
2023-07-08 16:52:59 +0300

    added templates for configuring hosts for ansible commands

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
 resource "virtualbox_vm" "node" {
   
-  count     = 1
   name      = "ubuntu-server" //format("node-%02d", count.index + 1)
   image     = "https://app.vagrantup.com/ubuntu/boxes/xenial64/versions/20211001.0.0/providers/virtualbox.box"
   cpus      = 2
-  memory    = "6144 mib"
-
-  # Copy in the bash script we want to execute.
-  provisioner "local-exec" {
-    interpreter = ["bash", "-c"]
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook create-users.yml dkb-playbook.yml -u vagrant -i '${self.network_adapter[0].ipv4_address},' --private-key ${var.vag_pvt_key} -e 'pub_key=.${var.vag_pub_key}'" //${var.playbook}
-  }
+  memory    = "6.0 gib"
 
   provisioner "remote-exec" {
 
-    inline = ["sudo apt-get update", "sudo apt install python-pip -y", "sudo add-apt-repository --yes --update ppa:ansible/ansible", 
+    inline = ["sudo apt-get update -y", "sudo apt install python-pip -y", "sudo add-apt-repository --yes --update ppa:ansible/ansible", 
     "sudo apt-get install ansible -y"]
 
     connection {
@@ -34,10 +27,29 @@ resource "virtualbox_vm" "node" {
 
 }
 
-output "Virtualbox_ip_addresses" {
-  value = {
-    for node in virtualbox_vm.node:
-    node.name => node.network_adapter[0].ipv4_address
+data "template_file" "dev_hosts" {
+  template = "${file("${path.module}/ansible/hosts.yml")}"
+  depends_on = [
+    virtualbox_vm.node
+  ]
+  vars = {
+    IP   = "${virtualbox_vm.node.network_adapter[0].ipv4_address}"
+    USER = "vagrant"
+    PK   = var.vag_pvt_key
   }
+}
+
+resource "null_resource" "dev-hosts" {
+  triggers = {
+    template_rendered = "${data.template_file.dev_hosts.rendered}"
+  }
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command = "echo '${data.template_file.dev_hosts.rendered}' > ./templates/dev_hosts.cfg && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook dkb-playbook.yml -u vagrant -i ./templates/dev_hosts.cfg --private-key ${var.vag_pvt_key} -e 'pub_key=.${var.vag_pub_key}'" //${var.playbook}
+  }
+}
+
+output "Virtualbox_ip_addresses" {
+  value = virtualbox_vm.node.network_adapter[0].ipv4_address
 }
 
